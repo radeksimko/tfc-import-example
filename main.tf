9d8954eaa5b60b77b0f6899144ebd4a9321aa9ec
Radek Simko <radek.simko@gmail.com>
2023-11-09 16:00:25 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
-output "foo" {
-  value = "barz"
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+import {
+  id = "supersecret"
+  to = random_password.password
+}
+
+resource "random_password" "password" {
+  length = 11
+  lower  = false
 }
